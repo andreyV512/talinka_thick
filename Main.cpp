b06f266d99efef03ac1ca715c59fd92b992f561d
thick <nobady@nomail.xx>
2018-09-25 12:40:12 +0500

    xxx

--- Main.cpp
+++ Main.cpp
@@ -301,8 +301,7 @@ void __fastcall TMainForm::bTestClick(TObject *Sender)
 	int TestTime=ini->ReadInteger("Default","TestTime_s",10);
 	//int TestZoneSize = ini->ReadInteger("Default", "TestZoneSize", 100);
 
-	//thickness.clear();
-	thickness.erase(thickness.begin(), thickness.begin()+thickness.size());
+	thickness.clear();
 	pr("Блокируем форму");
 	SetControlsAble(false,NULL,NULL);
 	ClearCharts();
